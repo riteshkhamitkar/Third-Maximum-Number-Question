d02b175d023430cca1a45d4de67cce1ffedf57fc
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-24 17:37:19 +0530

    Add files via upload

---
+++ Third Maximum Number Question (L-414).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet <Integer> set = new TreeSet<>();
+        for(int num : nums){
+            set.add(num);
+            if(set.size()>3){
+                set.pollFirst();
+            }
+        }
+        if(set.size()<3){
+            while(set.size()>1){
+                set.pollFirst();
+            }
+        }
+        return set.pollFirst();
+        
+    }
+}
